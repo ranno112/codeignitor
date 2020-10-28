1ede969208033f966f2aa1ccd9adcced703e5c94
Praktikant 10 <praktikant10@parnu.ee>
2020-10-28 13:54:06 +0200

    navbar tehtud postitused tehtud

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p> Version 2.0</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>Ruumid</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+	</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container">
+			<div class="navbar-header">
+		 	
+	 		<ul class="nav navbar-nav">
+	 		  <li> <a class="navbar-brand" href="<?php echo base_url(); ?>/">Ruumid</a></li>
+		      <li><a class="navbar-brand" href="<?php echo base_url(); ?>">Home </a></li>
+		      <li><a class="navbar-brand" href="<?php echo base_url(); ?>about">About </a></li>
+		      <li><a class="navbar-brand" href="<?php echo base_url(); ?>posts">Blog </a></li>
+		   </ul>
+		   </div>
+		 
+		</div>
+	</nav>
+
+
+<div class="container">
